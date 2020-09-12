5ed5764e28ea372e32ca9d54f787495031bfdb16
Aldy Madani <aldymadani10@gmail.com>
2020-09-12 20:41:20 +0800

    Adding manage booked appointment for doctor

--- DoctorWebApplication/DoctorAppointmentWebApplication/Controllers/DoctorController.cs
+++ DoctorWebApplication/DoctorAppointmentWebApplication/Controllers/DoctorController.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
 using DoctorAppointmentWebApplication.Models;
+using System.Diagnostics;
 
 namespace DoctorAppointmentWebApplication.Controllers
 {
@@ -142,6 +143,138 @@ namespace DoctorAppointmentWebApplication.Controllers
             }
             System.Diagnostics.Trace.WriteLine(appointment.ToString());
             return View(appointment);
+        }
+
+        // View Appointment of the Booked
+        public ActionResult ViewBookedAppointment()
+        {
+            CloudTable appointmentTable = GetTableInformation();
+            List<AppointmentEntity> appointments = new List<AppointmentEntity>();
+            var userId = userManager.GetUserId(HttpContext.User);
+            var user = userManager.GetUserAsync(User);
+            var userName = user.Result.Name;
+            try
+            {
+                TableQuery<AppointmentEntity> query =
+                    new TableQuery<AppointmentEntity>()
+                    .Where(TableQuery.CombineFilters(TableQuery.GenerateFilterCondition("PatientID", QueryComparisons.NotEqual, "None"),
+                           TableOperators.And,
+                           TableQuery.GenerateFilterCondition("DoctorID", QueryComparisons.Equal, userId)));
+                TableContinuationToken token = null;
+
+                do
+                {
+                    TableQuerySegment<AppointmentEntity> result = appointmentTable.ExecuteQuerySegmentedAsync(query, token).Result;
+                    token = result.ContinuationToken;
+
+                    foreach (AppointmentEntity patient in result.Results)
+                    {
+                        appointments.Add(patient);
+                    }
+                }
+                while (token != null);
+            }
+            catch (Exception e)
+            {
+                ViewBag.msg = "Error: " + e.ToString();
+            }
+
+            return View(appointments);
+        }
+
+        public ActionResult DeleteAppointment(string rowkey)
+        {
+            Trace.WriteLine("Delete is clicked" + rowkey);
+            /*if (!String.IsNullOrEmpty(HttpContext.Request.Query["rowkey"]))
+            {
+                rowkey = HttpContext.Request.Query["rowkey"];
+            }*/
+
+            CloudTable appointmentTable = GetTableInformation();
+            var msg = "";
+            TableOperation deleteAction = TableOperation.Delete(new AppointmentEntity("Appointment", rowkey) { ETag = "*" });
+            TableResult deleteResult = appointmentTable.ExecuteAsync(deleteAction).Result;
+
+            if (deleteResult.HttpStatusCode == 204)
+            {
+                msg = "Delete Succesfully";
+            }
+            else
+            {
+                msg = "Delete Failed Created By Patient";
+            }
+            return RedirectToAction("ViewBookedAppointment", "Doctor", new { msg });
+        }
+
+        public async Task<ActionResult> EditAppointment(string rowkey) // id = PartitionKey
+        {
+            CloudTable table = GetTableInformation();
+
+            // Create a retrieve operation that takes a item entity
+            TableOperation retrieveOperation = TableOperation.Retrieve<AppointmentEntity>("Appointment", rowkey);
+            //Execute the operation
+            TableResult retrievedResult = await table.ExecuteAsync(retrieveOperation);
+
+            // Assign the result to a Item object.
+            AppointmentEntity updateEntity = (AppointmentEntity)retrievedResult.Result;
+
+            /*if (updateEntity != null)
+            {
+                //Change the description
+                updateEntity.Description = "in nos";
+
+                // Create the InsertOrReplace TableOperation
+                TableOperation insertOrReplaceOperation = TableOperation.InsertOrReplace(updateEntity);
+
+                // Execute the operation.
+                table.Execute(insertOrReplaceOperation);
+                Console.WriteLine("Entity was updated.");
+            }*/
+
+
+            return View(updateEntity);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditAppointment(string PartitionKey, string RowKey, DateTime AppointmentDate, DateTime AppointmentTime)
+        {
+            bool hasChanged = false;
+            CloudTable table = GetTableInformation();
+
+            // Create a retrieve operation that takes a item entity
+            TableOperation retrieveOperation = TableOperation.Retrieve<AppointmentEntity>(PartitionKey, RowKey);
+            //Execute the operation
+            TableResult retrievedResult = await table.ExecuteAsync(retrieveOperation);
+
+            // Assign the result to a Item object.
+            AppointmentEntity updateEntity = (AppointmentEntity)retrievedResult.Result;
+
+            if (updateEntity != null)
+            {
+                //Change the description
+                if (updateEntity.AppointmentDate != AppointmentDate || updateEntity.AppointmentTime != AppointmentTime)
+                {
+                    hasChanged = true;
+                }
+                if (hasChanged)
+                {
+                    var appointmentDate = DateTime.SpecifyKind(AppointmentDate, DateTimeKind.Utc);
+                    updateEntity.AppointmentDate = appointmentDate;
+                    var appointmentTime = DateTime.SpecifyKind(AppointmentTime, DateTimeKind.Utc);
+                    updateEntity.AppointmentTime = appointmentTime;
+                    // Create the InsertOrReplace TableOperation
+                    TableOperation insertOrReplaceOperation = TableOperation.InsertOrReplace(updateEntity);
+
+                    // Execute the operation.
+                    await table.ExecuteAsync(insertOrReplaceOperation);
+                    Trace.WriteLine("Entity was updated.");
+                }
+                else
+                {
+                    Trace.WriteLine("No Changes");
+                }
+            }
+            return RedirectToAction("ViewBookedAppointment", "Doctor");
         }
     }
 }
